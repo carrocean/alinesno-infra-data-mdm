dca40ec3d7a9d05b407f3e7c51a14284977f260f
CarrotSea233 <2733614757@qq.com>
2023-08-18 15:32:07 +0800

    feat: 修正端口和集成demo

--- demo-mdm/src/main/java/com/alinesno/infra/base/mdm/demo/Application.java
+++ demo-mdm/src/main/java/com/alinesno/infra/base/mdm/demo/Application.java
@@ -1,19 +1,14 @@
-package com.alinesno.infra.data.mdm;
+package com.alinesno.infra.mdm.init.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
- * 集成一个Java开发示例工具
- * @author LuoAnDong
- * @since 2023年8月3日 上午6:23:43
- */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class BaseCmsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseCmsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
